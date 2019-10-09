db3fa16b25aa6e9364ded996bb5cf02e6d3e1fd1
Joshua Holloway <jthollo1@asu.edu>
2019-10-08 21:04:04 -0700

    Adding Junit test cases

--- Project 2/src/cse360assign2/AddingMachine.java
+++ Project 2/src/cse360assign2/AddingMachine.java
@@ -1,30 +1,90 @@
+//Assignment #: 2
+//Name        : Joshua Holloway
+//StudentID   : 1207270736
+//Lecture     : W 9:40-10:30am
+//Description : AddingMachine will add and subtract numbers from a stored int total
+//				and will also format the output into a single string.
+
 package cse360assign2;
 
-public class AddingMachine {
-
+public class AddingMachine 
+{
+	// declare variables
 	private int total;
-	
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+	private int count;
+	private String totalstr;
+
+	public AddingMachine() 
+	{
+		// initialize variables
+		total = 0;
+		count = 0;
+		totalstr = "";
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal()
+	{
+		return total;
 	}
 	
-	public void add (int value) {
+	public void add(int newvalue) // this method adds the input to the total and adds it to the string
+	{
+		total += newvalue;
+		buildString(currentOp("+") + newvalue); // made a buildString and currentOp method to better organize this process
+		count++; // keeping count of operations
+	}
+	
+	public void subtract(int newvalue) // this method subtracts the input from the total and adds it to the string
+	{
+		total -= newvalue;
+		buildString(currentOp("-") + newvalue);
+		count++;
+	}
+	
+	private String buildString(String newstr) // this method makes a complete string of our inputs
+	{									   // don't want a space before the first value
+		if(totalstr.length() != 0) 
+		{
+			newstr = " " + newstr;
+		}
 		
+		for(int i = 0; i < newstr.length(); i++) // inserting the built strings into a single string
+		{
+			totalstr += newstr.charAt(i);
+		}
+				
+		return totalstr;
 	}
 	
-	public void subtract (int value) {
+	private String currentOp(String newop) // this method determines the operator and additional space 
+	{									// that shows up before the input
+		String operator = "";
 		
+		if(count != 0)
+		{
+			operator = newop + " "; // adding a space to keep things tidy
+		}
+		else if(count == 0 && newop == "-") // added special case if user does .subtract on first value
+		{
+			operator = newop;
+		}
+		else // don't want an operator before the first value
+		{
+			operator = "";
+		}
+		
+		return operator;
 	}
 		
-	public String toString () {
-		return "";
+	public String toString() // no spaces after last value
+	{
+		return totalstr;
 	}
 
-	public void clear() {
-	
+	public void clear() // this method clears our variables
+	{
+		total = 0;
+		count = 0;
+		totalstr = "";	
 	}
 }
---
+++ Project 2/src/cse360assign2/AddingMachineTest.java
@@ -0,0 +1,93 @@
+//Assignment #: 2
+//Name        : Joshua Holloway
+//StudentID   : 1207270736
+//Lecture     : W 9:40-10:30am
+//Description : Class AddingMachineTest will test AddingMachine and compare it's toString()
+//				output to the expected output.
+
+package cse360assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddingMachineTest 
+{
+
+	@Test
+	void test1() 
+	{
+		String ans = "4 - 1 + 3";
+		AddingMachine Calc = new AddingMachine();
+		
+		Calc.add(4);
+		Calc.subtract(1);
+		Calc.add(3);
+
+		System.out.println(Calc.toString());
+		assertEquals(ans,Calc.toString());
+	}
+	
+	@Test
+	void test2() 
+	{
+		String ans = "-1 + 0 + 9";
+		AddingMachine Calc = new AddingMachine();
+		
+		Calc.subtract(1);
+		Calc.add(0);
+		Calc.add(9);
+		
+		System.out.println(Calc.toString());
+		assertEquals(ans,Calc.toString());
+	}
+	
+	@Test
+	void test3() 
+	{
+		String ans = "10 - 11 - 15 + 3";
+		AddingMachine Calc = new AddingMachine();
+		
+		Calc.add(10);
+		Calc.subtract(11);
+		Calc.subtract(15);
+		Calc.add(3);
+
+		System.out.println(Calc.toString());
+		assertEquals(ans,Calc.toString());
+	}
+	
+	@Test
+	void test4() 
+	{
+		String ans = "-4 - -1 + -3 + 3 - 2";
+		AddingMachine Calc = new AddingMachine();
+		
+		Calc.add(-4);
+		Calc.subtract(-1);
+		Calc.add(-3);
+		Calc.add(3);
+		Calc.subtract(2);
+
+		System.out.println(Calc.toString());
+		assertEquals(ans,Calc.toString());
+	}
+	
+	@Test
+	void test5() 
+	{
+		String ans = "400 - 1100 + 3200 - 1000 + -5500 - 0";
+		AddingMachine Calc = new AddingMachine();
+		
+		Calc.add(400);
+		Calc.subtract(1100);
+		Calc.add(3200);
+		Calc.subtract(1000);
+		Calc.add(-5500);
+		Calc.subtract(0);
+
+		System.out.println(Calc.toString());
+		assertEquals(ans,Calc.toString());
+	}
+
+}
